5cd81d2c5efafccfefd41c3e02d50e222885a70c
unknown <byachimowski@gmail.com>
2021-03-27 16:56:43 -0400

    Created project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set position to (Y=0, X=0, Z=0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+
+
+
+    }
+
+   void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+
+        if (transform.position.x > 11f)
+        {
+            transform.position = new Vector3(-11f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11f)
+        {
+            transform.position = new Vector3(11f, transform.position.y, 0);
+        }
+    }
+
+}
