7b9a37f0e045f3319e5f38cea295893ce95f1bee
byachimowski <80723906+byachimowski@users.noreply.github.com>
2021-11-22 11:17:16 -0500

    Frame Work 1 Secondary Fire Powerup Spawn Limiter

--- Assets/Scripts/Space Shooter/SpawnManager.cs
+++ Assets/Scripts/Space Shooter/SpawnManager.cs
@@ -14,7 +14,9 @@ public class SpawnManager : MonoBehaviour
     private GameObject[]Powerups;
     private GameObject _newEnemy;
     private UIManager _uiManager;
-
+    [SerializeField]
+    private int PwrUp,PwrUpCtn =0;// used to limit Secondary Fire Powerup spawning
+     
     public void StartSpawning()
     {
         StartCoroutine(SpawnRoutine());
@@ -43,14 +45,35 @@ public class SpawnManager : MonoBehaviour
         while(_enableSpawn)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-10f, 10f), 7f, 0);
-            //Instantiate(Powerups[2], posToSpawn, Quaternion.identity);
+            PwrUp = GetPowerUpID();
             Instantiate(Powerups[Random.Range(0,6)], posToSpawn, Quaternion.identity);
             yield return new WaitForSeconds(Random.Range(3, 8));
         }
 
 
     }
+   
+    private int GetPowerUpID()
+    {
+        int PUID = Random.Range(0, 6);
+        if(PUID ==5) //Player_Missile_Powerup
+        {
+            PwrUpCtn++;
+            if (PwrUpCtn >= 3) // limits the spawning of the Missile Power Up to once every 3 times it is selected
+            {
+                PwrUpCtn = 0;
+                return PUID; // return Id 5 to spawn a missile
+            }
+            else
+            {
+                PUID = Random.Range(0, 5);// only power ups 1-5 can spawn Not the Missile_Powerup
+                return PUID;
+            }
 
+            
+        }
+        return PUID;
+    }
 
     public void OnPlayerDeath()
     {
